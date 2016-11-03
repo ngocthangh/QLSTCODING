c024fa9c7ee4ca0eed2d0411be7f031b7b8d84b5
Nguyen <NguyenPC@DESKTOP-D88U8NK>
2016-11-03 14:26:42 +0700

    Test commit 2

--- QuanLiSieuThi/frmTrangChu.cs
+++ QuanLiSieuThi/frmTrangChu.cs
@@ -20,7 +20,7 @@ namespace QuanLiSieuThi
 
         private void frmTrangChu_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World 5!");
+            MessageBox.Show("Hello World 7!");
             DevExpress.UserSkins.BonusSkins.Register();
             //DevExpress.UserSkins.OfficeSkins.Register();
             DevExpress.XtraBars.Helpers.SkinHelper.InitSkinGallery(ribbonGalleryBarItem1, true);
